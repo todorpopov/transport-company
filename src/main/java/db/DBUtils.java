7ae23634c1faff972f9c1924f434a782c6f6cb57
tpopov <todorpopov02@gmail.com>
2025-01-19 18:03:13 +0200

    Configure Hibernate, PostgreSQL container, and DB utils methods

---
+++ src/main/java/db/DBUtils.java
@@ -0,0 +1,29 @@
+package db;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import org.hibernate.cfg.Configuration;
+
+public class DBUtils {
+    private static final SessionFactory sessionFactory;
+
+    static {
+        try {
+            sessionFactory = new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
+        } catch (Throwable ex) {
+            System.err.println("Initial SessionFactory creation failed: " + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static Session getCurrentSession() {
+        return sessionFactory.getCurrentSession();
+    }
+
+    public static void shutdown() {
+        if (sessionFactory != null) {
+            sessionFactory.close();
+        }
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,14 @@
 package org.example;
 
+import db.DBUtils;
+import org.hibernate.Session;
+
 public class Main {
     public static void main(String[] args) {
+        // Test
+
         System.out.print("Hello and welcome!");
+        Session session = DBUtils.getCurrentSession();
+        DBUtils.shutdown();
     }
 }
